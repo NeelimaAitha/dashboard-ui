24a14198234098b62bc7f0f790f46b8bc1eece8b
NeelimaAitha <138878450+NeelimaAitha@users.noreply.github.com>
2024-06-10 11:53:39 +0530

    Update Chart.js

--- components/Chart.js
+++ components/Chart.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import styles from '../styles/Chart.module.css';
 
-const Chart = () => {
+const Chart = ({ data }) => {
+  // Logic to render chart using 'data'
+
   return (
     <div className={styles.chart}>
-      {/* Chart component content */}
+      {/* Chart rendering logic */}
+      <svg width="400" height="200">
+        {/* Example chart content */}
+        <rect x="0" y="0" width="100" height="50" fill="#ff6347" />
+        <rect x="100" y="0" width="100" height="100" fill="#00bfff" />
+        <rect x="200" y="0" width="100" height="150" fill="#32cd32" />
+      </svg>
     </div>
   );
 };
